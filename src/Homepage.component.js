b5f25e22cbed16ff66d32132b7d1b9874751479e
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-02-19 17:59:49 +0100

    I build a first view of the homepage

--- src/Homepage.component.js
+++ src/Homepage.component.js
@@ -1,37 +1,43 @@
 import React from 'react';
 
+import './homepage.styles.scss';
+
 
 const HomePage = () => (
     <div className='homepage'>
         <div className="directory-menu">
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className='title'>SHOES</h1>
-                    <span className='subtitle'>SHOP NOW</span>
+            <div className="first-row">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className='title'>SHOES</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div> 
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className='title'>CLOTHES</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
                 </div>
-            </div> 
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className='title'>CLOTHES</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className='title'>ACCESORIES</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div> 
             </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className='title'>ACCESORIES</h1>
-                    <span className='subtitle'>SHOP NOW</span>
+            <div className="second-row">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className='title'>BALLS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
                 </div>
-            </div> 
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className='title'>BALLS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className='title'>OTHER</h1>
-                    <span className='subtitle'>SHOP NOW</span>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className='title'>OTHER</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
                 </div>
             </div>
         </div>
