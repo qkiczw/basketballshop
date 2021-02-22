d3dea18579aea8ec54521856d3f84e706b662273
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-02-22 19:12:59 +0100

    MenuItem was slightly rebuild and styled - bacground animation was added

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -3,9 +3,12 @@ import './menu-item.styles.scss';
 
 
 const MenuItem = ({ title, bgImgUrl, size}) => (
-    <div  className={`menu-item ${size}`} style={ {backgroundImage: `url(${bgImgUrl})`} }>
+    <div  className={`menu-item ${size}`} >
+        <div className="menu-background" style={ {backgroundImage: `url(${bgImgUrl})`} }>
+
+        </div>
         <div className="content">
-            <h1 className='title'>{title}</h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div> 
