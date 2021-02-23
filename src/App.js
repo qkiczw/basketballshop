48b38986e9502870cfc0013fdda8800590247eb8
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-02-23 20:57:01 +0100

    I added a route and a switch from react-router-dom and two dummy components dummy and header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 // Components
 import HomePage from './pages/homepage/Homepage.component';
+import Header from './components/header/Header.component';
+import Dummy from './pages/dummy/Dummy.comoponent';
 
 function App() {
   return (
-    <div> 
-      <HomePage />
+    <div>
+    <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/dummy' component={Dummy} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/dummy/Dummy.comoponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Dummy = () => (
+    <div>
+        <h1>Dummy component for Router Test</h1>
+    </div>
+)
+
+
+export default Dummy;
