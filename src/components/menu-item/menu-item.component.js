a8b4718df8dc7de41c1aa546e0d72b71286099cc
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-02-23 20:22:48 +0100

    React-router-dom was installed. BrowserRouter was wrapped around the app component. Small css chage was added - dark gray font and border color

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -4,9 +4,7 @@ import './menu-item.styles.scss';
 
 const MenuItem = ({ title, bgImgUrl, size}) => (
     <div  className={`menu-item ${size}`} >
-        <div className="menu-background" style={ {backgroundImage: `url(${bgImgUrl})`} }>
-
-        </div>
+        <div className="menu-background" style={ {backgroundImage: `url(${bgImgUrl})`} }></div>
         <div className="content">
             <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
