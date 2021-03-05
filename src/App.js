58e9643cec1480a9464f75ec9ef7376c518712d0
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-03-05 16:49:53 +0100

    App component in now changed to a Class component to store info about user in a state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
+// Import auht
+import { auth } from '../src/firebase/firebase.utilities';
+
 // Components
 import HomePage from './pages/homepage/Homepage.component';
 import Shop from './pages/shop/Shoppage.component';
@@ -9,17 +12,42 @@ import Header from './components/header/Header.component';
 import Dummy from './pages/dummy/Dummy.comoponent';
 import SingInAndSignUpPage from './pages/sign-in-and-sign-up-page/Sign-in-and-sign-up-page.component';
 
-function App() {
-  return (
-    <div>
-    <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={Shop} />
-        <Route path='/signin' component={SingInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
+      this.setState({ currentUser: user})
+
+    })
+  }
+  
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+
+  render() {
+    return (
+      <div>
+      <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={Shop} />
+          <Route path='/signin' component={SingInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
