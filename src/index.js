e388a44fe7ee7eebe59aec45591a64990f644de8
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-03-17 19:38:32 +0100

    Redux store was created

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
+
+const middlewares = [logger];
+
+cosnt store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export default store;
+
+
