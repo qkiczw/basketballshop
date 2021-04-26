1af68c1fe9b5dbb80f4c032c7a4a33179d44422f
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-04-26 12:36:16 +0200

    I`ve changed name of checkout page component, header of checkout page was created

--- src/pages/checkout/Checkout.component.js
+++ src/pages/checkout/Checkout.component.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './checkout.styles.scss';
 
 
-const Checkout = () => (
-    <div>
-        <h3>Checkout page!</h3>
+const CheckoutPage = () => (
+    <div className='checkout-page'>
+        <div className='checkout-header'>
+            <div className='header-block'>
+                <span>Produckt</span>
+            </div>
+            <div className='header-block'>
+                <span>Description</span>
+            </div>
+            <div className='header-block'>
+                <span>Price</span>
+            </div>
+            <div className='header-block'>
+                <span>Remove</span>
+            </div>
+        </div>
     </div>
 )
 
-export default Checkout;
+export default CheckoutPage;
