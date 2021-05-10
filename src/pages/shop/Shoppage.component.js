6aabe69a5fc7f36b0cece5a11debbc127ca97dac
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-05-10 15:08:23 +0200

    Just cleaning unnecessary disabled code

--- src/pages/shop/Shoppage.component.js
+++ src/pages/shop/Shoppage.component.js
@@ -4,36 +4,11 @@ import './shop.styles.scss';
 // Components
 import CollectionsOverview from '../../components/collections-overview/CollectionsOverview.component';
 
-// // Redux
-// import { connect } from 'react-redux';
-
-
-// // Selectrors
-// import { selectCollections } from '../../redux/shop/shop.selectors';
-// import { createStructuredSelector } from 'reselect';
-
-
-// // Components
-// import PreviewCollection from '../../components/preview-collection/Preview-collection.component';
-
-
-// const Shop = ({ collections }) => (
-//     <div className="shop-page">
-//         <h1>Our Collection</h1>
-//         {collections.map( ({id, ...restofCollectionProps}) => (
-//             <PreviewCollection key={id} {...restofCollectionProps}/>
-//         ))}
-//     </div>
-// )
 
 const Shop = () => (
     <div className="shop-page">
         <CollectionsOverview />
     </div>
 )
-
-// const mapSateTopProps = createStructuredSelector({
-//     collections: selectCollections
-// })
 
 export default Shop;
