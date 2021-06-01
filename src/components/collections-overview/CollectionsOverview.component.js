2ff82cc7399f49c69f11ec02b537c2e7eeac6cac
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-06-01 18:50:56 +0200

    CollectionOverview - css styles were replaced by styled components

--- src/components/collections-overview/CollectionsOverview.component.js
+++ src/components/collections-overview/CollectionsOverview.component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './collectionsOverview.styles.scss';
+// import './collectionsOverview.styles.scss';
 
 // Redux
 import { connect } from 'react-redux';
@@ -11,13 +11,16 @@ import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 // Components
 import PreviewCollection from '../../components/preview-collection/Preview-collection.component';
 
+// Styled Components
+import { CollectionsOverviewContainer } from './collection-overview.styles';
+
 const CollectionsOverview = ({ collections }) => (
-    <div className='collections-overview'>
+    <CollectionsOverviewContainer>
         <h1>Our Collection</h1>
         {collections.map( ({id, ...restofCollectionProps}) => (
             <PreviewCollection key={id} {...restofCollectionProps}/>
         ))}
-    </div>
+    </CollectionsOverviewContainer>
 )
 
 const mapSateTopProps = createStructuredSelector({
