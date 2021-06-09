d5b63189edb4c41468f4e4e4c45b123ff74a9607
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-06-09 19:11:37 +0200

    Shoppage component - css styles were replaced by styled components

--- src/pages/shop/Shoppage.component.js
+++ src/pages/shop/Shoppage.component.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './shop.styles.scss';
 
 // Router
 import { Route } from 'react-router-dom';
@@ -9,14 +8,16 @@ import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/CollectionsOverview.component';
 import CollectionPage from '../collection/collection.component';
 
+// Styled Components
+import { ShopPageContainer } from './shoppage.styles';
 
 const Shop = ({ match }) => (
-    <div className="shop-page">
+    <ShopPageContainer>
         {/* <CollectionsOverview /> */}
         {/* { match.path - much flexible than hard coded '/shop' path } */}
         <Route exact path={`${match.path}`} component={CollectionsOverview}/> 
         <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
-    </div>
+    </ShopPageContainer>
 )
 
 export default Shop;
--- src/pages/shop/shoppage.styles.js
+++ src/pages/shop/shoppage.styles.js
@@ -1,7 +1,9 @@
-.shop-page {
+import styled from 'styled-components';
+
+export const ShopPageContainer = styled.div`
     width: 100%;
     padding: 0 100px;
     display:  flex;
     flex-direction: column;
     gap: 100px;
-}
+`;
